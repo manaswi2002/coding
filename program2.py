ec9b8092800e16df66a5f58046221e51d47b138b
manaswi2002 <142235828+manaswi2002@users.noreply.github.com>
2023-08-28 23:09:46 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,3 +1,4 @@
+#Write a program to calculate the amount to be paid after 8 years,where principal=5000,rate of intrest per annum =9.
 P= float(input('Enter the principal:'))
 R= float(input('Enter the rate of intrest per annum:'))
 T=float(input(;Enter the time in years:'))
