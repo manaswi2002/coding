1daa6ff9cb8a5b0771f8a7abfd1e24a18137478d
manaswi2002 <142235828+manaswi2002@users.noreply.github.com>
2023-08-28 23:06:33 +0530

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,6 @@
+P= float(input('Enter the principal:'))
+R= float(input('Enter the rate of intrest per annum:'))
+T=float(input(;Enter the time in years:'))
+SI=(P*R*T)/100
+amount=SI+P
+print('Total amount:',amount)
